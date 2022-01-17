2c9c9e32bc7c96e014c1eddb9a000e4cfbf76aff
nghi-nguyen-beep <nghin@mit.edu>
2022-01-17 14:23:01 -0600

    Add font to home page

--- src/components/CircleButton/elements.js
+++ src/components/CircleButton/elements.js
@@ -1,11 +1,22 @@
 import styled from 'styled-components';
-  
+import {createGlobalStyle} from "styled-components"
+import Ramaraja from "../../pages/includes/fonts/Ramaraja/Ramaraja.woff";
+
+
+export const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: Ramaraja;
+    src: url(${Ramaraja}) format('woff');
+  }
+`;
+
 export const Button = styled.button`
     border-radius: 100%;
-    background-color: ${props => props.color ? props.color : "white"};
+    background-color: ${props => props.color ? props.color : "black"};
     color: white;
     font-size: 36px;
-    height: 350px;
-    width: 350px;
+    height: 320px;
+    width: 320px;
     border: none;
+    font-family: 'Ramaraja';
 `;
--- src/components/CircleButton/index.js
+++ src/components/CircleButton/index.js
@@ -1,10 +1,13 @@
-import { Button } from "./elements";
+import { Button, GlobalStyles } from "./elements";
 import { useNavigate } from "react-router-dom";
 
 function CircleButton(props) {
     const navigate = useNavigate();
     return (
-        <Button color={props.color} onClick={() => navigate("/" + props.route)}>{props.route}</Button>
+        <div className="CircleButton-main">
+            <GlobalStyles />
+            <Button color={props.color} onClick={() => navigate("/" + props.route)}>{props.route}</Button>
+        </div>
     );
 };
 
