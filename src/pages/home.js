804dbf3d0a2ab67bf14dcce8e1fa4681d0590b38
nghi-nguyen-beep <nghin@mit.edu>
2022-01-17 14:50:33 -0600

    Align circle buttons on home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,9 +4,13 @@ import CircleButton from '../components/CircleButton';
 function Home() {
     return (
         <div className= "home-main">
-          <CircleButton color= "#DA7272" route="art"></CircleButton>
-          <CircleButton color= "#A9C3EB" route="about"></CircleButton>
-          <CircleButton color= "#F0D07D" route="projects"></CircleButton>
+          <div className='home-row-one'>
+            <CircleButton color= "#A9C3EB" route="about"></CircleButton>
+          </div>
+          <div className='home-row-two'>
+            <CircleButton color= "#DA7272" route="art"></CircleButton>
+            <CircleButton color= "#F0D07D" route="projects"></CircleButton>
+          </div>
         </div>
       );
 };
