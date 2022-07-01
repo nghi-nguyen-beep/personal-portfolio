a424f8bb4bdfef8131da3a98f07da9a14be8ae26
nghi-nguyen-beep <nghin@mit.edu>
2022-07-01 00:24:50 -0400

    Add about

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,7 @@
 import './includes/css/about.css';
 import PageAnimate from '../components/PageAnimate';
 import resume from '../assets/about/NghiNguyenResume_1:25.pdf';
+import profile from '../assets/about/profile-picture.webp';
 
 function About() {
     return (
@@ -10,11 +11,17 @@ function About() {
             <div className="about-title">
               about me
             </div>
-            <div className="about-description">
-              {/* <div>hulo, here are some facts about me below.</div> */}
-              <div>Download my resume <a href={resume} download="NghiNguyenResume">here</a></div>
-
+          </div>
+          <div className="about-profile-picture">
+            <img className= "about-image" src= {profile} alt= "profile" style={{width: "18vw", height: "18vw", borderRadius: "50%", transform: "rotate(41deg)"}}/>
+          </div>
+          <div className="about-description">
+            <div>
+              <p>Welcome to my portfolio! My name is Nghi (pronounced "ne:ə") and I'm a current senior studying CS at MIT with interests in design, HCI, AI, and languages.<br></br> 
+             In my free time, I love to doodle, play tennis, tutor, and explore bakeries / Vietnamese restaurants whereever I am. <br></br>
+              I created this website for the intent of relearning React and also using it as a journal to keep myself accountable for staying on track with drawing. Feel free to look around!</p> <br></br>
             </div>
+            <div>Download my resume (last updated Jan 25, 2022) <a href={resume} download="NghiNguyenResume">here</a></div>
           </div>
         </div>
       </PageAnimate>
